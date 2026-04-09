edc3dec8e3ead32cd741ea81ce053a4c22f087ca
AbhiStark780 <abhistark57@gmail.com>
2026-04-10 00:51:34 +0530

    added hangman game

---
+++ PROJECTS/wordslist.py
@@ -0,0 +1,43 @@
+# words for hangman game
+
+# list id generated using gemini
+
+words = [
+    # Animals & Nature
+    "elephant", "giraffe", "kangaroo", "dolphin", "penguin", 
+    "octopus", "thunder", "volcano", "glacier", "blossom",
+    "badger", "falcon", "lizard", "sequoia", "meadow",
+    
+    # Technology & Science
+    "computer", "keyboard", "internet", "circuit", "robotic", 
+    "science", "gravity", "molecule", "fission", "galaxy",
+    "neutron", "silicon", "antenna", "battery", "scanner",
+    
+    # Household & Food
+    "blanket", "kitchen", "pancake", "toaster", "cupboard", 
+    "curtain", "omni-bus", "spatula", "avocado", "pretzel",
+    "napkin", "refrigerator", "closet", "window", "bedroom",
+
+    # Abstract & Verbs
+    "journey", "silence", "freedom", "whisper", "victory", 
+    "imagine", "patience", "believe", "dynamic", "mystify",
+    "shimmer", "puzzled", "vibrant", "fragile", "bravery",
+
+    # Tricky (Hard letters: X, Z, Q, J)
+    "jukebox", "quartz", "zodiac", "wizard", "galaxy", 
+    "oxygen", "luxury", "vortex", "injury", "banjo",
+    "quiver", "hazard", "puzzling", "jackpot", "equinox",
+
+    # The "Villains" (Short, no vowels, or strange spelling)
+    "rhythm", "syzygy", "lynx", "phlegm", "crypt", 
+    "nymph", "gypsy", "myrrh", "abyss", "psyche",
+    "queue", "gnome", "khaki", "sphinx", "yacht",
+
+    # Travel & Places
+    "airport", "station", "highway", "bicycle", "subway", 
+    "pyramid", "island", "castle", "safari", "tourist",
+    
+    # Miscellaneous
+    "captain", "diamond", "feather", "guitar", "hammer", 
+    "jacket", "lantern", "magnet", "needle", "orange"
+]
